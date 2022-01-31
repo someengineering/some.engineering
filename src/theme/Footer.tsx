1727ba3fcc90a8bbf8758169b489137f4535db41
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2022-01-31 08:23:19 -0500

    fix: re-render Scarf pixel on route change

--- src/theme/Footer.tsx
+++ src/theme/Footer.tsx
@@ -1,13 +1,20 @@
+import { useLocation } from '@docusaurus/router';
 import useIsBrowser from '@docusaurus/useIsBrowser';
 import Logo from '@site/static/img/logo-md.svg';
 import OriginalFooter from '@theme-original/Footer';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Footer(): JSX.Element {
   const isBrowser = useIsBrowser();
   const isDev =
     process.env.NODE_ENV == 'development' ||
     (isBrowser && window.location.href.includes('localhost'));
+
+  const [timestamp, setTimestamp] = useState(new Date().getTime());
+  const location = useLocation();
+  useEffect(() => {
+    setTimestamp(new Date().getTime());
+  }, [location]);
 
   return (
     <>
@@ -17,7 +24,7 @@ export default function Footer(): JSX.Element {
       ) : (
         <>
           <img
-            src="https://static.scarf.sh/a.png?x-pxid=7341e2b9-4c79-40ee-b2a9-c4b5ad3b9fb3"
+            src={`https://static.scarf.sh/a.png?x-pxid=7341e2b9-4c79-40ee-b2a9-c4b5ad3b9fb3&${timestamp}`}
             alt=""
             style={{ height: '1px', width: '1px' }}
           />
